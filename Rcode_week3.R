c161421257a7d8385dad3da9c784c23f32b86dd0
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2025-07-25 15:28:16 +1200

    Update Rcode_week3.R

--- Rcode_week3.R
+++ Rcode_week3.R
@@ -2,3 +2,102 @@
 
 sat <- read.csv("active-satellites.csv")
 str(sat)
+
+head(sat)
+
+#print the first 3 rows
+sat[1:3,]
+#print the variable names
+names(sat)
+
+#taking a look at the first few values of period..minutes
+sat$Period..Minutes.[1:3]
+
+#check if these are characters
+is.character(sat$Period..Minutes.)
+
+#this will cause a NAs warning
+sat$PeridMin <- as.numeric(sat$Period..Minutes.)
+
+#find which characters are NA
+sat$Period..Minutes.[is.na(sat$PeriodMin)]
+
+#fix this data, the process of data cleaning
+sat$PeriodMin[sat$Period..Minutes.=="8 days"] <- 8*24*60
+
+
+#now lets calculate the mean period
+mean(sat$PeriodMin) #we getting NA because of the values. 
+#let's exclude these values by removing NAs before giving
+#Periods to mean()
+mean(sat$PeriodMin[!is.na(sat$PeriodMin)])
+
+#or use ask mean() to remove NAs for us
+mean(sat$PeriodMin, na.rm=TRUE)
+
+#let's plot a simple barplot!
+barplot(table(sat$Launch.Site))
+
+#graph does not look that good, we only have some labels, and also good to have these in order of increasing number of launches
+#get at able of numbers of launches by site
+tablaunch <- table(sat$Launch.Site)
+
+#sort it into increasing order
+tablaunch <- sort(tablaunch)
+#draw a barplot, rotate the names, and reduce font size
+barplot(tablaunch, las=2, cex.names=0.4)
+
+#check for the 5 biggest launch sites
+rev(tablaunch)[1:5]
+
+#functions quick overview
+square <- function(x) {
+  #square the value
+  return(x^2)
+}
+
+#implement the function and square 2
+square(x=2)
+#alternatively we can do this:
+square(2)
+
+#here we can square a scalar
+aa <-6
+square(aa)
+
+aa <- c(1,4,5)
+square(aa)
+
+
+
+#square the elements of the array
+aa <- array(c(1,4,5,3,3,3), dim=c(3,2))
+square(aa)
+
+
+#check the arguments of square()
+#this will output null
+args(square)
+
+#args median has one default argument
+args(median)
+
+v <- c(3, 4, NA, 10)
+v
+
+
+#any math operation involving NA gives NA, so this result isn't helpful.
+# we can get R to ignore the missing value, so we can call median in various ways
+median(v, TRUE)
+
+#positional matching of the arguments
+#we can write more readable code and be explicit about these arguments by naming them
+median(x=v, na.rm=TRUE)
+
+#now this is called explicit matching, then the order doesnt matter
+median(na.rm=TRUE, x=v)
+
+#in many R functions, there are one or two arguments that are required, followed
+#By a whole lot of optional arguments
+args(legend)
+
